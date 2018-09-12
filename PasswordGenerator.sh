35b42b08b565724db406b3b6d8df93b5097f4cce
Alexis Ahmed <alexiayub34@gmail.com>
2018-09-12 08:36:45 +0300

    Add files via upload

---
+++ PasswordGenerator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Simple Password Generator 
+
+echo "This is a simple password generator"
+echo "PLease enter the length of the password: "
+read PASS_LENGTH
+
+for p in $(seq 1 5);
+do
+	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+
+done
+
+
