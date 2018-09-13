cfff51156252861be8b696f46eb42e38a341bd72
Kr1ss <kr1ss.x@yandex.com>
2018-09-13 16:47:12 +0200

    restrict input to positive integers
    This commit adds a loop in order to repeat printing the prompt in case
    something other than a positive integer value is entered, effectively
    enforcing the correct input format.

--- PasswordGenerator.sh
+++ PasswordGenerator.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
+#
 # Simple Password Generator
+#
 
-echo "This is a simple password generator"
-echo "Please enter the length of the password: "
-read PASS_LENGTH
+echo This is a simple password generator
+read -p 'Please enter the length of the password: ' userinput
 
+# remove all non-digits from the input
+PASS_LENGTH="${userinput//[!0-9]}"
+
+# if invalid, repeat until the user enters a valid positive integer
+while [ "$PASS_LENGTH" != "$userinput" ]
+do
+	printf '\nPassword length should be a positive integer, obviously...\n'
+    read -p 'Please enter the length of the password: ' userinput
+	PASS_LENGTH="${userinput//[!0-9]}"  # remove all non-digits
+done
+
+# password generation and output
+printf '\n\nList of generated passwords\n\n'
 for p in $(seq 1 5)
 do
-	tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c "$PASS_LENGTH"
+	tr -dc 'A-Za-z0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'\''' </dev/urandom | head -c $PASS_LENGTH
 	echo
 done
+echo
