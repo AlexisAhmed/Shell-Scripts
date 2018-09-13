e580dae60a91bd1b6069b462ac0193f41c890fe5
Kr1ss <kr1ss.x@yandex.com>
2018-09-13 17:04:18 +0200

    let the user enter how many passwords he needs
    Here, we add another prompt to ask the user for the number of passwords
    he actually wants.
    This value is ensured to be a positive int the same way we do it for
    PASS_LENGTH, and used as upper limit in the final genaration-/output-
    loop.

--- PasswordGenerator.sh
+++ PasswordGenerator.sh
@@ -5,22 +5,33 @@
 #
 
 echo This is a simple password generator
-read -p 'Please enter the length of the password: ' userinput
+read -p 'How many passwords will you need ? ' userinput
 
-# remove all non-digits from the input
-PASS_LENGTH="${userinput//[!0-9]}"
+# remove all digits from input
+NUMBER="${userinput//[!0-9]}"
 
 # if invalid, repeat until the user enters a valid positive integer
+while [ "$NUMBER" != "$userinput" ]
+do
+	printf '\nAn amount should be a positive integer, obviously...\n'
+    read -p 'How many passwords will you need: ' userinput
+	NUMBER="${userinput//[!0-9]}"  # remove all non-digits
+done
+echo
+
+# now let's do the same procedure to get the desired password length
+read -p 'Please enter the length of the passwords: ' userinput
+PASS_LENGTH="${userinput//[!0-9]}"
 while [ "$PASS_LENGTH" != "$userinput" ]
 do
 	printf '\nPassword length should be a positive integer, obviously...\n'
     read -p 'Please enter the length of the password: ' userinput
-	PASS_LENGTH="${userinput//[!0-9]}"  # remove all non-digits
+	PASS_LENGTH="${userinput//[!0-9]}"
 done
 
 # password generation and output
 printf '\n\nList of generated passwords\n\n'
-for p in $(seq 1 5)
+for p in $(seq 1 $NUMBER)
 do
 	tr -dc 'A-Za-z0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'\''' </dev/urandom | head -c $PASS_LENGTH
 	echo
