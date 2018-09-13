32e901a723d2bcd9da702b390b1c22f5576c75a9
Kr1ss <kr1ss.x@yandex.com>
2018-09-13 15:44:30 +0200

    use a POSIX shell instead of Bash
    A simple Bourne shell (resp. Bash in POSIX mode) is perfectly alright
    for our purpose, so no need to open a full Bash here.

--- PasswordGenerator.sh
+++ PasswordGenerator.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-#Simple Password Generator 
+# Simple Password Generator
 
 echo "This is a simple password generator"
 echo "Please enter the length of the password: "
 read PASS_LENGTH
 
-for p in $(seq 1 5);
+for p in $(seq 1 5)
 do
 	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
-
 done
-
-
