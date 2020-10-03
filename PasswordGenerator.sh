1a16c82c7e76dbd945ddb4f337f3a6ed5051bef0
Baljeet Singh <prince.singh4000@gmail.com>
2020-10-03 12:05:41 -0400

    Update PasswordGenerator.sh

--- PasswordGenerator.sh
+++ PasswordGenerator.sh
@@ -2,14 +2,17 @@
 
 #Simple Password Generator 
 
-echo "This is a simple password generator"
-echo "Please enter the length of the password: "
-read PASS_LENGTH
+echo "Simple Base64 password generator: "
 
-for p in $(seq 1 5);
-do
-	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
-
+echo "Enter length of the password: "
+read FINAL_LENGTH
+                                                                                                     
+echo "Enter number of passwords you want: "                                                          
+read PASS_NUMBER                                                                                     
+                                                                                                     
+for s in $(seq 1 $PASS_NUMBER);                                                                      
+do                                                                                                   
+        openssl rand -base64 48 | cut -c1-$FINAL_LENGTH                                              
+                                                                                                     
 done
-
 
