08d1b4c5b4d63a28030c0b0aab624e1a24eafb90
Shayan Paul <92013663+0xinf3rn0@users.noreply.github.com>
2021-12-18 00:47:38 +0530

    Update PasswordGenerator.sh
    This can be more prominent scrip for generating passwords...

--- PasswordGenerator.sh
+++ PasswordGenerator.sh
@@ -5,8 +5,9 @@
 echo "This is a simple password generator"
 echo "Please enter the length of the password: "
 read PASS_LENGTH
+read -p "How many password you want?: " COUNT
 
-for p in $(seq 1 5);
+for p in `seq $COUNT`;
 do
 	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
 
