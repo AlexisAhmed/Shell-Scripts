8f01fa45841e95503bd66e5f26cea8f56c50b1a9
k3shav17 <keshavrao0489@gmail.com>
2022-03-01 16:28:37 +0530

    pass_gen_updated

--- PasswordGenerator.sh
+++ PasswordGenerator.sh
@@ -1,15 +1,61 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-#Simple Password Generator 
 
-echo "This is a simple password generator"
-echo "Please enter the length of the password: "
-read PASS_LENGTH
+#####################################################################
+##########		PASSWORD GENERATOR	 	#############
+#####################################################################
 
-for p in $(seq 1 5);
+
+echo "Password Generator"
+## ASKING THE USER FOR THE SITE WHICH HE WANTS GENERATE A PASSOWRD
+echo "Enter the sitename for which you want to generate a password"
+read SITENAME
+
+## WHAT LENGTH THE PASSWORD NEEDS TO BE GENERATED
+echo "Please Enter the length of the password"
+read PASSSIZE
+
+## VARIABLES TO STORE THE PASSWORD 
+FIRSTHALF=""
+SECONDHALF=""
+PASSLENG=`expr $PASSSIZE / 2`
+
+
+## LOOPING ONE TIME FOR GENERATING THE PASSWORD. 
+## YOU CAN CHANGE THE NUMBER IN THE SEQ TO ANY NUMBER WHICH GENERATES THAT NUMBER OF PASSWORDS, FROM WHICH YOU CAN SELECT.
+for p in $(seq 1);
 do
-	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
-
+	FIRSTHALF=`openssl rand -base64 48 | cut -c1-$PASSLENG` 
+	SECONDHALF=`openssl rand -base64 48 | cut -c1-$PASSLENG`
 done
+
+## STORING THE PASSWORDS TO A TEXT FILE FOR LATER REFERENCE
+## UNCOMMENT THE BELOW LINE IF YOU WANTS TO STORE THE PASSWORDS
+echo "$SITENAME:	$FIRSTHALF$SECONDHALF" >> passwords.txt
+
+## PRINTING THE GENERATED PASSWORD TO THE CONSOLE
+echo "$SITENAME:	$FIRSTHALF$SECONDHALF"
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
